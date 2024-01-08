46570878766e11b707e9cc258223d08afd1ba740
Q4119-Midnight <tanrivermis.mehmet@gmail.com>
2024-01-09 01:29:24 +0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../helper/logo192.png";
 
 const Header = () => {
   return (
-    <div className=" container d-flex  p-5 align-items-center">
+    <div className=" container d-flex  p-5 align-items-center justify-content-center">
       <div>
         <img
           style={({ width: "100px" }, { height: "100px" })}
